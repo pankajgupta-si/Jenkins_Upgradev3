da236e4abcbe671cafb4d527294c3f0952fa36c1
pankajgupta-si <guptapankaj855@gmail.com>
2020-12-29 01:16:04 +0000

    neww checkout

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetinghappyMessage() {
     String someone = "World, Thanks you";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
